e90b2e50e64bb4a34af72747be25173785ebcb03
VoidIfPerceived <sophi@offnominal.com>
2023-09-05 21:58:42 -0500

    mid video edits

--- menuApp.js
+++ menuApp.js
@@ -9,8 +9,8 @@ What you create is up to you as long as it meets the following requirements:
 //Golf Course Menu App -- Create, view, and delete course data (maybe even edit?????)
 //Needs To Track -- Hole Number / Distance to Hole / Par Number / Personal Hole Record
 
-class Hole {
-    constructor(holeNum, distance, strokes) {
+class Hole { //Creates a new class "Hole"
+    constructor(holeNum, distance, strokes) { //Constructor for hole class
         this.holeNum = holeNum;
         this.distance = distance;
         this.strokes = strokes;
@@ -18,13 +18,13 @@ class Hole {
 
     thisHole() {
         return `Hole ${holeNum} is a ${this.distance} yard, par ${this.strokes} hole.`;
-    }
+    } // returns info about the hole
 }
 
-class Course {
-    constructor(name) {
+class Course { //Creates a new class "Course"
+    constructor(name) { //Constructor for Course Class
         this.name = name;
-        this.holes = [];
+        this.holes = []; //Creates a new holes array for the course object created by the course class
     }
 
     addHole(hole) {
@@ -32,24 +32,24 @@ class Course {
             this.holes.push(hole);
         }   else {
             throw new Error(`You may only add an instance of hole. Argument: "${hole}" is not a hole`);
-        }
+        } //Checks if a "hole" is added (not called), throws error if not a "hole"
     }
 
     thisCourse() {
         return `${this.name} has ${this.holes.length} holes.`;
-    }
+    } // returns info about the course
 }
 
 class Menu {
     constructor() {
-        this.courses = [];
-        this.selectedCourse = null;
+        this.courses = []; //Creates a new courses array for the main menu to use to keep a list of courses
+        this.selectedCourse = null; //Declares an empty selectedCourse variable for the menu (used to track which course selected from courses array)
     }
 
     start() {
-        let selection = this.showMenuOptions();
+        let selection = this.showMenuOptions(); //selection is whatever is returned from the showMenuOptions prompt
         while (selection != 0) {
-            switch (selection) {
+            switch (selection) { //switch case used to check menu functions
                 case '1':
                     this.createCourse();
                     break;
@@ -70,7 +70,7 @@ class Menu {
         alert('Goodbye!');
     }
 
-    showMenuOptions() {
+    showMenuOptions() { //Prompts user to input a number for one of these outcomes
         return prompt(`Please enter one of the following numbers to continue:
         0) Exits the program
         1) Create a new course
@@ -80,7 +80,7 @@ class Menu {
         `);
     }
 
-    showHoleMenuOptions(courseInfo) {
+    showHoleMenuOptions(courseInfo) { //Prompts user for input number (further into the menus)
         return prompt(`Please enter one of the following numbers to continue:
         0) Return to Main Menu
         1) Create a new hole
@@ -91,25 +91,25 @@ class Menu {
     }
 
 
-    createCourse() {
+    createCourse() { //Prompts and pushes a new course object into the courses array
         let name = prompt('Enter new course name:');
         this.courses.push(new Course(name));
     }
 
-    viewCourse() {
+    viewCourse() { //Prompts for the index of a course, displays the info about that course and its holes
         let index = prompt('Enter the index of the course you wish to view:');
         if (index >= 0 && index < this.courses.length) { //Validating user's input
             this.selectedCourse = this.courses[index];
             let courseData = 'Course Name: ' + this.selectedCourse.name + '\n';
 
-            for (let i = 0; i < this.selectedCourse.holes.length; i++) {
+            for (let i = 0; i < this.selectedCourse.holes.length; i++) { //Input validation
                 courseData += i + ') Hole ' + this.selectedCourse.holes[i].holeNum + ' - ' 
                     + this.selectedCourse.holes[i].distance + ' Yards - ' 
                     + 'Par ' + this.selectedCourse.holes[i].strokes + '\n';
             }
 
             let selection = this.showHoleMenuOptions(courseData);
-            switch (selection) {
+            switch (selection) { //Switch case to use the input given in showHoleMenuOptions
                 case '1':
                     this.createHole();
                     break;
@@ -119,32 +119,35 @@ class Menu {
         }
     }
 
-    deleteCourse() {
-
+    deleteCourse() { //Allows for the deletion of a course (user specifies index)
+        let index = prompt(`Enter the index of the course you wish to delete:`);
+        if (index >= 0 && index < this.courses.length) { //Input verification
+            this.courses.splice(index, 1); //Deletes the value at index (, 1 means only that value is deleted)
+        }
     }
 
-    displayCourses() {
+    displayCourses() { //Displays the courses which have already been created
         let courseString = '';
         for (let i = 0; i < this.courses.length; i++) {
             courseString += i + ') ' + this.courses[i].name + '\n';
         }
         alert(courseString);
     }
-    createHole() {
+    createHole() { //Prompts to create a hole, asks for the information for the hole, then pushes a new object using that info to the holes array for that course
         let holeNum = prompt(`Enter the hole number (1 - 18):`);
         let distance = prompt(`Enter the distance from the tee to the hole (in yards):`);
         let strokes = prompt(`Enter the number of strokes required to reach par on this hole:`);
         this.selectedCourse.holes.push(new Hole(holeNum, distance, strokes));
     }
 
-    deleteHole() {
+    deleteHole() { //Allows for the deletion of the hole at index specified by user
         let index = prompt('Enter the index of the hole you wish to delete');
-        if (index >= 0 && index < this.selectedCourse.holes.length) {
-            this.selectedCourse.holes.splice(index, 1);
+        if (index >= 0 && index < this.selectedCourse.holes.length) { //Input validation
+            this.selectedCourse.holes.splice(index, 1); //Deletes the value at index (, 1 means only that value is deleted)
         }
     }   
     
 }
 
-let menu = new Menu;
-menu.start();
+let menu = new Menu; //Creates a new menu object which is runnable (not runnable as a class)
+menu.start(); //Runs the start() method from the menu object created above
